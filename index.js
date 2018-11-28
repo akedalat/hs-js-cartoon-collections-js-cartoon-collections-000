0b8f4cd2bb0b9be79753734336e9768700dda269
Kian Edalat <akedalat@gmail.com>
2018-11-28 17:15:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 function dwarfRollCall(dwarves) {
+
+var dwarveNames = `1. ${dwarves[0]} `
+for (let i = 1; i < dwarves.length; i++) {
+dwarveNames += `${i+1}. ${dwarves[i]} `
+
+}
+return dwarveNames
 }
 
 function summonCaptainPlanet(planeteerCalls){
